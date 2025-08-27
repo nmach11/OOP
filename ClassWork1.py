d674709551f3e4ef3193847c06049eb1431c9615
nmach11 <nmach11@outlook.com>
2025-08-27 13:54:04 -0500

    Add files via upload

---
+++ ClassWork1.py
@@ -0,0 +1,7 @@
+employee_name = input("Enter the employee name: ")
+wages = int(input("Enter the wages: "))
+hours = int(input("Enter hours: "))
+days = int(input("Enter days: "))
+
+monthly_pay = wages*hours*days
+print(monthly_pay)
